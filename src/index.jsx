dec0f033243c0d66b377b818893ce5fe3e2062ea
Sasha Bajzek <dabridginator@gmail.com>
2016-07-14 12:25:43 -0700

    Added the store

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {fromJS, List, Map} from 'immutable';
+import makeStore from './src/store';
 
 import HaikuApp from './components/HaikuApp';
+
+export const store = makeStore();
 
 const haikus = fromJS(
 	[{id: 1, haikuLine1: "trying to wake up", haikuLine2: "coffee is my addiction", haikuLine3: "how i love it so", haikuTheme: "coffeeTheme"},
