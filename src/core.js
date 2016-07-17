fbf30efc6f86edbd77ef50ca6c3de29d5c692b0b
Sasha Bajzek <dabridginator@gmail.com>
2016-07-17 09:21:53 -0700

    Randomize initial haikus

--- src/core.js
+++ src/core.js
@@ -2,11 +2,29 @@ import {fromJS, List, Map} from 'immutable';
 
 export const INITIAL_STATE = Map();
 
+var shuffle = function (array) {
+	var currentIndex = array.length, temporaryValue, randomIndex;
+	//While there remain elements to shuffle
+	while (0 !== currentIndex) {
+		//Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -=1;
+		//And swap it with the current element
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+	return array;
+};
+
 export function setHaikus(state, haikus) {
-	return state.set('haikus', fromJS(haikus))
+	
+	//Add randomizer for haikus array
+	var haikusRandom = shuffle(haikus);
+	
+	return state.set('haikus', fromJS(haikusRandom))
 							.set('currentId', 0);
 }
-
 
 export function next(state) {
 	
