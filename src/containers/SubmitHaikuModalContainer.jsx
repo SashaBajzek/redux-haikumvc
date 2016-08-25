8ab09b58e6adfa1fe718f34109c87a73c7579143
Sasha Bajzek <dabridginator@gmail.com>
2016-08-25 12:19:09 -0700

    Deleted unnecessary imports

--- src/containers/SubmitHaikuModalContainer.jsx
+++ src/containers/SubmitHaikuModalContainer.jsx
@@ -1,10 +1,7 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { Button, ControlLabel, FormControl, FormGroup, Modal, show, onHide, keyboard, enforceFocus, bsClass, dialogComponentClass, dialogClassName, closeButton } from 'react-bootstrap';
 import { closeModal, addHaiku } from '../action_creators';
 import { SubmitHaikuModal } from '../components/SubmitHaikuModal';
-import { initialize } from 'redux-form';
-
 
 const handleSubmit = function(data) {
 	console.log('Submission received!', data);
